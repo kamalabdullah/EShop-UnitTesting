dc050c07f707e902663dc7cfd4f64b6669665a28
kamal abdullah <kamal.abdullah@integrant.com>
2019-11-27 13:37:51 +0200

    Adding MS Test

---
+++ Integrant.EShop.Services.Test.MSTest/ProductServiceTest.cs
@@ -0,0 +1,46 @@
+﻿using Integrant.EShop.Data.Contracts.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integrant.EShop.Services.Test.MSTest
+{
+    [TestClass]
+    public class ProductServiceTest
+    {
+        private ProductService productService;
+
+       [TestInitialize]
+        public void TestInitialize()
+        {
+            var productRepoMock = new Mock<IProductRepository>();
+            productRepoMock.Setup(s => s.GetProductById(It.IsAny<int>())).ReturnsAsync(new Data.Contracts.DTOs.ProductInfo());
+            productService = new ProductService(productRepoMock.Object);
+        }
+        [DataTestMethod]
+        [DataRow(1)]
+        [DataRow(2)]
+        public async Task GetProductByIdShouldNotNullTest(int Id)
+        {
+            var product = await productService.GetProductById(Id);
+            Assert.IsNotNull(product);
+        }
+        [DataTestMethod]
+        [DataRow(-1)]
+        [DataRow(0)]
+        public async Task GetProductByIdShouldBeNullTest(int Id)
+        {
+            var product = await productService.GetProductById(Id);
+            Assert.IsNull(product);
+        }
+        [TestMethod]
+        public void ShouldThrowExceptionIfProductRepositoryIsNull()
+        {
+            var exception = Assert.ThrowsException<ArgumentNullException>(() => new ProductService(null));
+            Assert.AreEqual("productRepository", exception.ParamName);
+        }
+    }
+}
---
+++ Integrant.EShop.Services.Test.MSTest/UserServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using Integrant.EShop.Data.Contracts.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Integrant.EShop.Services.Test.MSTest
+{
+    [TestClass]
+    public class UserServiceTest
+    {
+        private UserService userService;
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            var userRepoMock = new Mock<IUserRepository>();
+            userService = new UserService(userRepoMock.Object);
+        }
+        [TestMethod]
+        public void ShouldReturnNullIfUserNameEmpty()
+        {
+            var user = userService.GetUserByName("");
+            Assert.IsNull(user);
+        }
+        [TestMethod]
+        public void ShouldThrowExceptionIfUserRepositoryIsNull()
+        {
+            var exception = Assert.ThrowsException<ArgumentNullException>(() => new UserService(null));
+            Assert.AreEqual("userRepository", exception.ParamName);
+        }
+    }
+}
