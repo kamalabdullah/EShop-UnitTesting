365aa59d85bdbd55cd79288427926282e2176dc4
kamal abdullah <kamal.abdullah@integrant.com>
2019-11-27 13:49:38 +0200

    adding NUnit Test

---
+++ Integrant.EShop.Services.Test.NUnit/ProductServiceTest.cs
@@ -0,0 +1,44 @@
+﻿using Integrant.EShop.Data.Contracts.Repositories;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integrant.EShop.Services.Test.NUnit
+{
+    [TestFixture]
+    public class ProductServiceTest
+    {
+        private ProductService productService;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            var productRepoMock = new Mock<IProductRepository>();
+            productRepoMock.Setup(s => s.GetProductById(It.IsAny<int>())).ReturnsAsync(new Data.Contracts.DTOs.ProductInfo());
+            productService = new ProductService(productRepoMock.Object);
+        }
+        [TestCase(1)]
+        [TestCase(2)]
+        public async Task GetProductByIdShouldNotNullTest(int Id)
+        {
+            var product = await productService.GetProductById(Id);
+            Assert.IsNotNull(product);
+        }
+        [TestCase(-1)]
+        [TestCase(0)]
+        public async Task GetProductByIdShouldBeNullTest(int Id)
+        {
+            var product = await productService.GetProductById(Id);
+            Assert.IsNull(product);
+        }
+        [Test]
+        public void ShouldThrowExceptionIfProductRepositoryIsNull()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(() => new ProductService(null));
+            Assert.AreEqual("productRepository", exception.ParamName);
+        }
+    }
+}
---
+++ Integrant.EShop.Services.Test.NUnit/UserServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using Integrant.EShop.Data.Contracts.Repositories;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Integrant.EShop.Services.Test.NUnit
+{
+    [TestFixture]
+    public class UserServiceTest
+    {
+        private UserService userService;
+        [SetUp]
+        public void TestInitialize()
+        {
+            var userRepoMock = new Mock<IUserRepository>();
+            userService = new UserService(userRepoMock.Object);
+        }
+        [Test]
+        public void ShouldReturnNullIfUserNameEmpty()
+        {
+            var user = userService.GetUserByName("");
+            Assert.IsNull(user);
+        }
+        [Test]
+        public void ShouldThrowExceptionIfUserRepositoryIsNull()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(() => new UserService(null));
+            Assert.AreEqual("userRepository", exception.ParamName);
+        }
+    }
+}
